69280d2d03516ebd819d4c78db203bf3831b123a
ChristopherPayn <christopher.payn@hotmail.co.uk>
2023-05-29 22:34:41 +0100

    first layout and data manipulation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import { mockMeals } from './mock-data';
 import './App.css';
 
-function App() {
+const Meal = ({ meal }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='meal'>
+      <span className='meal__name'>{meal.name}</span>
+      <span className='meal__quantity'>{meal.quantity}</span>
     </div>
   );
-}
+};
+
+const MealList = ({ meals }) => {
+  return (
+    <div className='meal-list'>
+      <span>Meal List</span>
+      {meals.map(meal => (
+        <Meal key={meal.id} meal={meal} />
+      ))}
+    </div>
+  )
+};
+
+const MealTime = ({ mealTime, meals, handleMealSelected }) => {
+  let currentMeal = '';
+  const handleSelectChange = (event) => {
+    const { value: newMeal } = event.target;
+    handleMealSelected(currentMeal, newMeal);
+  };
+
+  const handleFocus = (event) => {
+    console.log('FOCUS:', event.target.value);
+    currentMeal = event.target.value;
+  };
+
+  return (
+    <div className='meal-time'>
+      {mealTime}
+      <select onFocus={handleFocus} onChange={handleSelectChange}>
+        <option>Select...</option>
+        {meals.map(meal => {
+          console.log('MAP RUN');
+          if(meal.quantity > 0) {
+            return <option key={meal.id}>{meal.name}</option>
+          }
+        })}
+      </select>
+    </div>
+  );
+};
+
+const Day = ({ date, meals, handleMealSelected }) => {
+  return (
+    <div className='day'>
+      <div>{date}</div>
+      <div className='meal-times'>
+        <MealTime mealTime='Breakfast' meals={meals} handleMealSelected={handleMealSelected} />
+        <MealTime mealTime='Lunch' meals={meals} handleMealSelected={handleMealSelected} />
+        <MealTime mealTime='Dinner' meals={meals} handleMealSelected={handleMealSelected} />
+      </div>
+    </div>
+  );
+};
+
+const App = () => {
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    setMeals(mockMeals);
+  }, []);
+
+  const handleMealSelected = (previousMeal, selectedMeal) => {
+    const newMeals = meals.map((meal) => {
+      if (meal.name === previousMeal) {
+        console.log('PREVIOUS MEAL:', meal)
+        return {
+          ...meal,
+          quantity: meal.quantity + 1,
+        };
+      };
+      if (meal.name === selectedMeal) {
+        console.log('SELECTED MEAL:', meal)
+        return {
+          ...meal,
+          quantity: meal.quantity - 1,
+        };
+      };
+      return meal;
+    });
+    console.log({ newMeals });
+    setMeals(newMeals);
+  };
+
+  return (
+    <div>
+      App
+      <MealList meals={meals} />
+      <Day date={'Monday'} meals={meals} handleMealSelected={handleMealSelected} />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/mock-data.js
@@ -0,0 +1,27 @@
+export const mockMeals = [
+  {
+    id: 1,
+    name: 'Stew',
+    quantity: 6,
+  },
+  {
+    id: 2,
+    name: 'Hotpot',
+    quantity: 3,
+  },
+  {
+    id: 3,
+    name: 'Biryani',
+    quantity: 4,
+  },
+  {
+    id: 4,
+    name: 'Some big long crazy dish',
+    quantity: 4,
+  },
+  {
+    id: 5,
+    name: 'Limited Meal',
+    quantity: 1,
+  },
+];
